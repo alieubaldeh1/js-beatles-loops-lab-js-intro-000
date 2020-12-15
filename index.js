57a0822f880718a15470f2069437a54b6ea547ef
Alieu Baldeh <alieubaldeh@gmail.com>
2020-12-15 15:56:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
-function theBeatlesPlay () {
 
+function theBeatlesPlay (musicians, instruments) {
+  let arr = new Array; 
+  for (let n = 0; n < musicians.length; n ++) { 
+    arr.push(`${musicians[i]} plays ${instruments[n]}`)
+  }
+  return arr
+
+}
+
+function johnLennonFacts (strings) {
+  // return strings+'!'
+}
+
+function iLoveTheBeatles (num) { 
+//   if (num === 7 ) { 
+//   return "I love the Beatles" * 8
+// } else if (num === 17) { 
+// return ["I love the Beatles"] 
+//   }
+// }
 }
